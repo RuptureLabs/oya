ba91d87cc6aac3345194f975d02c2e295d112c85
Edimedia Mutoke <kilufyamutoke@gmail.com>
2024-01-21 23:41:26 +0200

    Fix application generator

--- src/oya/core/management/commands/runserver.py
+++ src/oya/core/management/commands/runserver.py
@@ -2,6 +2,7 @@
 
 import sys
 from uvicorn import main
+from oya.conf import settings
 from oya.core.management.base import BaseCommand
 from oya.core.management.utils import print_banner
 
@@ -14,5 +15,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print_banner()
         sys.argv = sys.argv[1:]
-        sys.argv.insert(1, "oya.apps.main:app")
+        sys.argv.insert(1, settings.ASGI_APPLICATION)
         sys.exit(main()) # ignore: E1120
